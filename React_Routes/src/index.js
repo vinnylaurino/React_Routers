bab6dfdb50b1c8d50840e9b6952b27bd627897d1
vinnylaurino <vinny.laurino@gmail.com>
2018-04-09 19:28:07 -0400

    added routes and styling

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
 
@@ -6,13 +8,32 @@ import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
-class Home extends Component{
-  render(){
-    return(
-      // your code goes here
-    )
-  }
-}
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+const Home = () => {
+    return(
+    <Router>
+      <div>
+      <nav id="nav">
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/sleepy"> Sleepy </Link>
+          </li>
+          <li>
+            <Link to="/happy"> Happy </Link>
+          </li>
+          <li>
+            <Link to="/guilty"> Guilty </Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/happy" component={Happy} />
+      <Route path="/sleepy" component={Sleepy} />
+      <Route path="/Guilty" component={Guilty} />
+      </div>
+    </Router>
+  );
+};
+render( <Home />, document.querySelector('#root') );
